40a6280269fa8795c84abc3e2c5caef2357d4461
Philippe VIENNE <philippegeek@gmail.com>
2017-10-31 07:15:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Secure-In browser extension
+
+## How to build it
+
+ - `npm install`
+ - `npm run build`
+
+The WebExtension in the [addon](addon/) folder should now work.
+
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        background_scripts: "./src/background_scripts/background.js",
+        popup: "./src/popup/left-pad.js"
+    },
+    module: {
+
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        'scss': 'vue-style-loader!css-loader!sass-loader',
+                        'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+                    }
+                    // other vue-loader options go here
+                }
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin('addon'),
+        new CopyWebpackPlugin([
+            {from: 'rsr'}
+        ])
+    ],
+    output: {
+        path: path.resolve(__dirname, "addon"),
+        filename: "[name]/index.js"
+    }
+};
